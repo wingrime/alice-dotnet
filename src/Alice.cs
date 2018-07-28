8bc09875cacf72b9dfe782735ca4cab861e92952
wingrime <wingrime@gmail.com>
2018-07-28 15:12:56 +0500

    a

--- src/Alice.cs
+++ src/Alice.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
@@ -14,12 +15,30 @@ public class Alice : Controller {
       .ConfigureServices(srv => srv.AddMvc())
       .Configure(app => app.UseMvc());
 
-  static Replies _replies = new Replies {
-    ["привет хай здравствуй здравствуйте алиса"] = x => x.Reply("Привет"),
-    ["как дела делишки"] = x => x.Reply("Хорошо, а у тебя?"),
-    ["*"] = x => x.Reply("Я не знаю что тебе сказать")
+ // static Replies _replies = new Replies {
+ //   ["Какой цвет подойдет к желтому"] = x => x.Reply("К желтому подойдет синии или розовый"),
+ //   [""] = x => x.Reply("Хорошо, а у тебя?"),
+ //   ["*"] = x => x.Reply("Я не знаю что тебе сказать")
+ // };
+  Dictionary<string, string> replies = new Dictionary<string,string>()
+  {
+    { "желтый", "ключ 1"},
+    { "зеленый", "2"},
+    { "синии ", "3"}
   };
+   
 
   [HttpPost("/alice")]
-  public AliceResponse WebHook([FromBody] AliceRequest req) => _replies.Match(req);
+  public AliceResponse WebHook([FromBody] AliceRequest req)
+  {
+    var str = req.Request.Command.Split(' ');
+
+    var s = str.FirstOrDefault(x => replies.ContainsKey(x));
+
+    if (s != null)
+      return req.Reply(replies[s]);
+    
+    return  req.Reply("я не знаю такой цвет");
+    
+  }
 }
